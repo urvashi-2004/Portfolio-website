2b7332de706ccb9c191699b7eb557b4f1a36a317
urvashi-2004 <85677323+urvashi-2004@users.noreply.github.com>
2023-08-22 14:36:56 +0530

    added

--- index.js
+++ index.js
@@ -45,3 +45,20 @@ $('.carousel').owlCarousel({
 
 
 })
+
+
+
+
+
+var typeData = new Typed(".role", {
+    strings: [
+       "Full Stack Developer",
+       "Web Developer",
+       "Software Developer",
+       "Coder",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+  });
